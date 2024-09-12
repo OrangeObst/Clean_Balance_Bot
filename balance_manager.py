62b51475149872e003794fec89b4294e40387ac8
OrangeObst <tobiascoombs@gmail.com>
2024-09-12 20:53:07 +0200

    Slight clean up of debug code

--- balance_manager.py
+++ balance_manager.py
@@ -9,6 +9,8 @@ import mputest
 from smbus2 import SMBus 
 
 
+_DEBUG = False
+
 class LowPassFilter:
 		def __init__(self, alpha):
 			self.alpha = alpha
@@ -22,14 +24,8 @@ class LowPassFilter:
 class Speed_Calculator(object):
 	# 			(		P, I, D,  min_out, 		max_out, 	 	balance_point)
 	def __init__(self, kp, ki, kd, min_out=-100, max_out=100, balance_point=0):
-		self.timer = Timer("test", text="Control loop: {milliseconds:.6f} ms")
-		self.timer2 = Timer("test2", text="Control loop2: {milliseconds:.6f} ms")
-		self.timer3 = Timer("test3", text="Control loop3: {milliseconds:.6f} ms")
-		self.timer4 = Timer("test4", text="Control loop4: {milliseconds:.6f} ms")
-		self.timer5 = Timer("test5", text="Control loop5: {milliseconds:.6f} ms")
-
 		'''
-		--------------- PID values ---------------
+		-------------------- PID values --------------------
 		'''
 		self.kp = kp
 		self.ki = ki
@@ -49,41 +45,42 @@ class Speed_Calculator(object):
 		self.iterms = manager.list()
 		self.dterms = manager.list()
 
-
 		'''
-		--------------- IMU init ---------------
+		-------------------- IMU init --------------------
 		'''
-
+		# MPU with bus 1, digital low pass filter 2 and samplerate divisor 4
 		bus = SMBus(1)
 		self.mpu = mputest.MyMPU6050(bus)
 		
 		self.mpu.set_dlpf_cfg(2)
 		self.mpu.set_smplrt_div(4)
 
+		# Low pass filter
 		lpf_alpha = 0.1		# For low pass filter
 		self.lpf_x = LowPassFilter(lpf_alpha)
 		self.lpf_y = LowPassFilter(lpf_alpha)
 		self.lpf_z = LowPassFilter(lpf_alpha)
 
-		accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z = self.mpu.MPU_ReadData()
-		
+
 		self.pitch_offset = 0.9
 		self.balance_point = balance_point
 		
 		self.imu_data = manager.list()
 		
-		self.alpha = 0.95		# For complementary filter
+		self.alpha = 0.96		# For complementary filter
 
+		# Set initial values
 		accel_readings = np.array([self.mpu.MPU_ReadData() for _ in range(10)])
 		accel_avg = np.mean(accel_readings, axis=0)
 		self.initial_pitch = math.degrees(math.atan2(-accel_avg[0], math.sqrt(accel_avg[1]**2 + accel_avg[2]**2)))
+		# self.initial_pitch = self.initial_pitch - self.pitch_offset
+		
 		self.previous_error = self.balance_point - self.initial_pitch
 		self.previous_pitch = self.initial_pitch
 
 
 	# @Timer(name="Control Loop", text="Control loop: {milliseconds:.6f}ms")
 	def control_loop(self):
-		# self.timer.start()
 		'''
 		 --------------- Collect and process IMU data ---------------
 		'''
@@ -98,11 +95,10 @@ class Speed_Calculator(object):
 		accel_y = self.lpf_y.filter(accel_y)
 		accel_z = self.lpf_z.filter(accel_z)
 
-		# Pitch stablefrom accelerometer
+		# Pitch from accelerometer
 		pitch_from_acceleration = math.degrees(math.atan2(-accel_x, math.sqrt(accel_y**2 + accel_z**2)))
-		# pitch_from_acceleration = math.degrees(math.atan2(accel_x,accel_z))
 		
-		# Integrate gyro data to get pitch change
+		# Pitch from gyroscope
 		pitch_gyro_integration = self.previous_pitch + gyro_y * dt
 		print(f'Pitch from acc: {pitch_from_acceleration}, Pitch from gyro: {pitch_gyro_integration}')
 		
@@ -113,14 +109,16 @@ class Speed_Calculator(object):
 
 		self.imu_data.append(current_angle)
 		self.previous_pitch = pitch
-		# self.timer4.stop()
+
 		'''
 		--------------- Update PID controls ---------------
 		'''
-		# self.timer5.start()
+
+		# Get error state
 		self.error = self.balance_point - current_angle
 		self.sum_error += self.error * dt
 
+		# Calculate PID terms
 		pterm = self.kp * self.error
 		self.pterms.append(pterm)
 		iterm = self.ki * self.sum_error
@@ -131,18 +129,9 @@ class Speed_Calculator(object):
 
 		output = max(self.min_out, min(self.max_out, output))
 		print(f'Error: {self.error} = balance_point {self.balance_point} - corr pitch {current_angle}')
-		# print(f'pterm: {pterm}, iterm {iterm} dterm {dterm} output {output}')
+
 		self.previous_error = self.error
 
 		self.pid_data.append(output)
-		# self.timer5.stop()
-		# self.timer.stop()
-		return output
 
-	def mean_time(self):
-		# print(f'Mean: {self.timer.timers.mean("test")}, stdev: {self.timer.timers.stdev("test")}')
-		# print(f'Mean2: {self.timer.timers.mean("test2")}, stdev2: {self.timer.timers.stdev("test2")}')
-		# print(f'Mean3 {self.timer.timers.mean("test3")}, stdev3: {self.timer.timers.stdev("test3")}')
-		# print(f'Mean4: {self.timer.timers.mean("test4")}, stdev4: {self.timer.timers.stdev("test4")}')
-		# print(f'Mean5: {self.timer.timers.mean("test5")}, stdev5: {self.timer.timers.stdev("test5")}')
-		pass
+		return output
